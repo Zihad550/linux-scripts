859a2e44c4c9975cd55c070c7f3e1952f063dbac
Zihad550 <dev.jehadhossain@gmail.com>
2023-01-09 09:37:28 +0600

    add applications

---
+++ applications.sh
@@ -0,0 +1,42 @@
+# download updates
+sudo apt -y update
+sudo apt -y upgrade
+# install applications
+sudo apt -y install ubuntu-restricted-extras git vlc curl openssh-server ruby-full gnome-shell-extensions gnome-tweaks neofetch
+# install snaps
+sudo snap install --classic code
+sudo snap install brave
+sudo snap install postman
+# sudo snap install pycharm-community --classic
+# install nvm and node.js stable
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+nvm install 14
+nvm install 16
+
+# install package managers
+# install yarn
+corepack enable
+npm i -g corepack
+yarn set version stable
+# install pnpm
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+
+# ohter development packages
+# nodemon
+npm install -g nodemon
+# heroku cli
+curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
+
+# customize ubuntu
+sudo apt-add-respository ppa:papirus/papirus
+sudo apt -y install papirus-icon-theme
+
+# install obs studio
+#sudo add-apt-repository -y ppa:obsproject/obs-studio
+#sudo apt update
+#sudo apt install -y obs-studio
+
+# install kden live
+#sudo add-apt-repository -y ppa:kdenlive/kdenlive-stable
+#sudo apt update
+#sudo apt install -y kdenlive
--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 echo "Setting up nvm!"
 sudo apt install curl 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 16
 npm i -g npm@latest
-npm install -g zx
-source ~/.bashrc
-zx -v
-zx https://raw.githubusercontent.com/Zihad550/linux-scripts/main/index.mjs
+#npm install -g zx
+#source ~/.bashrc
+#zx -v
+#zx https://raw.githubusercontent.com/Zihad550/linux-scripts/main/index.mjs
