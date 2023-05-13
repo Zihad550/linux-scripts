39ada5d89a9a429b1561c854b2d297033a3d0266
Zihad550 <dev.jehadhossain@gmail.com>
2023-05-13 07:57:05 +0600

    s

--- fedora/applications.sh
+++ fedora/applications.sh
@@ -7,21 +7,22 @@ sudo dnf -y install git vlc curl openssh-server gnome-shell-extensions gnome-twe
 flatpak install com.brave.Browser
 flatpak install com.getpostman.Postman
 flatpak install com.mongodb.Compass
-sudo dnf -y install thunderbird
+flatpak install com.getmailspring.Mailspring
+# sudo dnf -y install thunderbird
 # sudo snap install pycharm-community --classic
 # install nvm and node.js stable
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+curl https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 nvm install 14
 nvm install 16
 
 # install package managers
 # install yarn
-corepack enable
-npm i -g corepack
-yarn set version stable
+# corepack enable
+# npm i -g corepack
+# yarn set version stable
 # install pnpm
 curl -fsSL https://get.pnpm.io/install.sh | sh -
 
 # ohter development packages
 # nodemon
-npm install -g nodemon
+# npm install -g nodemon
