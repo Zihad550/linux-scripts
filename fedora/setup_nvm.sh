2904095b3738e9b5f04c0dc493403a08b151bf0b
Zihad550 <dev.jehadhossain@gmail.com>
2023-05-13 08:18:00 +0600

    update nvm

--- fedora/setup_nvm.sh
+++ fedora/setup_nvm.sh
@@ -1,6 +1,6 @@
 echo "Setting up nvm!"
 sudo dnf install curl 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 #export NVM_DIR="$HOME/.nvm"
 #[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 #[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
